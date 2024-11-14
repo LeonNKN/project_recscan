b7e0518ef54e17c396174419b4ff1fb87352b4de
LEE LI XIONG <leelixiongmatt@gmail.com>
2024-11-14 23:57:51 +0800

    [ Featrue + UI ] Open Camera. -Testing. UI Upgrade

--- recscan/lib/main.dart
+++ recscan/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+import 'pages/scan_page.dart';
+import 'pages/profile_page.dart';
+import 'widgets/custom_nav_bar.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,10 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Custom Navigation Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Floating Navigation Bar',
+      theme: ThemeData(primarySwatch: Colors.purple),
       home: MainPage(),
     );
   }
@@ -26,9 +28,9 @@ class _MainPageState extends State<MainPage> {
   int _selectedIndex = 0;
 
   final List<Widget> _pages = [
-    Center(child: Text('Home Page')),
-    Center(child: Text('Camera Page')),
-    Center(child: Text('Profile Page')),
+    HomePage(),
+    ScanPage(),
+    ProfilePage(),
   ];
 
   void _onItemTapped(int index) {
@@ -40,61 +42,10 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Custom Navigation Example'),
-      ),
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: _pages,
-      ),
-      bottomNavigationBar: Container(
-        color: Colors
-            .blue.shade50, // Set the background color of the navigation bar
-        padding:
-            EdgeInsets.symmetric(vertical: 10), // Add padding to adjust height
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            _buildNavItem(Icons.home, 'Home', 0),
-            _buildNavItem(Icons.camera, 'Camera', 1),
-            _buildNavItem(Icons.person, 'Profile', 2),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildNavItem(IconData icon, String label, int index) {
-    final isSelected = _selectedIndex == index;
-
-    return GestureDetector(
-      onTap: () => _onItemTapped(index),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          AnimatedContainer(
-            duration: Duration(milliseconds: 300),
-            width: isSelected ? 60 : 40, // Larger when selected
-            height: isSelected ? 60 : 40,
-            decoration: BoxDecoration(
-              color: isSelected ? Colors.blue : Colors.transparent,
-              shape: BoxShape.circle,
-            ),
-            child: Icon(
-              icon,
-              size: isSelected ? 30 : 24,
-              color: isSelected ? Colors.white : Colors.black,
-            ),
-          ),
-          SizedBox(height: 4), // Space between icon and label
-          Text(
-            label,
-            style: TextStyle(
-              color: isSelected ? Colors.blue : Colors.black,
-              fontSize: isSelected ? 14 : 12,
-            ),
-          ),
-        ],
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: CustomNavBar(
+        selectedIndex: _selectedIndex,
+        onItemTapped: _onItemTapped,
       ),
     );
   }
