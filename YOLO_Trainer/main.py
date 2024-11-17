d7117afcfcdb0198bf94d7579b96999fcc8de346
LEE LI XIONG <leelixiongmatt@gmail.com>
2024-11-18 00:18:42 +0800

    [ Feature ] Added Model onnx

--- YOLO_Trainer/main.py
+++ YOLO_Trainer/main.py
@@ -5,7 +5,7 @@ trainer = YOLOTrainer(
     model_path="yolo11n.pt",
     yaml_path=fr"C:/Users/USER/Desktop/YOLO/2024/2024/data.yaml" # Path to dataset.yaml
 )
-trainer.train(epochs=2, img_size=640, batch_size=16)
+trainer.train(epochs=50, img_size=640, batch_size=16)
 
 
 evaluation_results = trainer.evaluate()
@@ -13,4 +13,4 @@ print(evaluation_results)
 
 new_receipt_path = "receipt.jpg"
 inference_results = trainer.predict(new_receipt_path, save_results=True)
-#trainer.export_model(export_format="onnx")
+trainer.export_model(export_format="onnx")
